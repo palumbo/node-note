9c8b57d9f51d5c04af038e1f40eb2734f9c7f166
Joseph Palumbo <palumbo@Josephs-MacBook-Pro.local>
2019-02-08 06:47:05 -0600

    completed video 10

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-console.log("Starting app"); 
+console.log("Starting app.js"); 
 
 const fs = require('fs'); 
 const os = require('os'); 
+const notes = require('./notes.js'); 
 
-var user = os.userInfo(); 
+var sum = notes.add(2, 3); 
+console.log(sum); 
+
+/* var user = os.userInfo(); 
  
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
     if (err) {
         console.log('Unable to write to file'); 
     }
-})
+}) */
