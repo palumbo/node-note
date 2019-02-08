54beb76344c53b66dbb5893a01eae056439a312a
Joseph Palumbo <palumbo@Josephs-MacBook-Pro.local>
2019-02-08 07:34:13 -0600

    installing modules from npm

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ console.log("Starting app.js");
 
 const fs = require('fs'); 
 const os = require('os'); 
+const _ = require('lodash'); 
 const notes = require('./notes.js'); 
 
-var sum = notes.add(2, 3); 
-console.log(sum); 
+var filteredArray = _.uniq(["Joseph", 1, "Joseph", 1, 2, 3, 4]);
+console.log(filteredArray); 
+
+/* var sum = notes.add(2, 3); 
+console.log(sum);  */
 
 /* var user = os.userInfo(); 
  
