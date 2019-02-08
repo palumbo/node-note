8ecdf290589859a90a1b2a72809595a9cce5cd3a
Joseph Palumbo <palumbo@Josephs-MacBook-Pro.local>
2019-02-08 07:42:14 -0600

    added nodemon, cleaned up app.js

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 console.log("Starting app.js"); 
 
 const fs = require('fs'); 
-const os = require('os'); 
 const _ = require('lodash'); 
+
 const notes = require('./notes.js'); 
-
-var filteredArray = _.uniq(["Joseph", 1, "Joseph", 1, 2, 3, 4]);
-console.log(filteredArray); 
-
-/* var sum = notes.add(2, 3); 
-console.log(sum);  */
-
-/* var user = os.userInfo(); 
- 
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
-    if (err) {
-        console.log('Unable to write to file'); 
-    }
-}) */
